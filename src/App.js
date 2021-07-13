e8e85b8389151e40f09aa8f6dab5875f7fc079ba
Majestic.cloud <hello@majestic.cloud>
2021-07-13 23:22:22 +0300

    add welcome header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <h1>Welcome</h1>
         <p>
           This is just a sample React app that we're going to deploy
         </p>
