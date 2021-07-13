5855e3f3d313b1d7cbcaff939db929bc80f94bb5
Majestic.cloud <hello@majestic.cloud>
2021-07-13 10:03:37 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is just a sample React app that we're going to deploy
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
