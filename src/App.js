2dd5ea1dac89653b6729eebd74e5ce6652107072
sherrylqq <81358785+sherrylqq@users.noreply.github.com>
2022-10-04 11:27:10 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Welcome</h1>
+          <h1>Welcome to my react</h1>
           <h2>My React Deployment pipeline is ready</h2>
         <p>
           This is just a sample React app that we're going to deploy
